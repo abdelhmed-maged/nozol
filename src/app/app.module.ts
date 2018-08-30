28ca30da65dfeda45111de7fc56ca5fb637d0ca8
Mohamed El-Feky <elfeky.m@gmail.com>
2018-08-30 20:35:57 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
+import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material';
+import { MatButtonModule, MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
-
+import { DataTableComponent } from './data-table/data-table.component';
+import { PropertyService } from './services/property.service';
+//import { UsertableComponent } from './components/usertable/usertable.component';
+import { PropertyTableComponent } from './components/property-table/property-table.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DataTableComponent,
+    //UsertableComponent,
+    PropertyTableComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PropertyService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
