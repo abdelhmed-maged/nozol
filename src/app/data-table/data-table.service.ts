fe126db4144522a37fcc7983f863721866a81258
ishift942 <ishift.cs@hotmail.com>
2018-08-31 21:05:42 +0200

    test

--- src/app/data-table/data-table.service.ts
+++ src/app/data-table/data-table.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import {Headers, Http, RequestOptions, Response, ResponseContentType} from '@angular/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataTableService {
 
-  constructor() { }
+  constructor(private http: Http,) { }
+
+  updateRequestStatus(id: number, status: string, reason?: string, note?: string, completion_time?: number, completion_time_text?: string) {
+
+
+    return this.http.post( '/api/update-request', {
+      id: id,
+      status: status
+     
+    })
+      .map((response: Response) => response.json());
+  }
 }
